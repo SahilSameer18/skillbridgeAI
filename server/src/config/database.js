285972515c67a0b09e27a36713ec7d24fec039ff
sahil sameer siddique <sahilsameer.dev18@gmail.com>
2026-03-25 18:48:40 +0530

    update packages

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose');
 
 
-async function connectToDB(){
+async function connectToDB() {
   try {
     await mongoose.connect(process.env.MONGO_URI)
     console.log('connected to Database')
   }
-  catch (err){
+  catch (err) {
     console.log(err)
   }
-  
+
 }
 
 module.exports = connectToDB
